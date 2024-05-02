1660e2f9919bbd4d332553671082ec0fc553e5c8
Frank <12001003032399@FEDIDCARD.GOV>
2024-05-02 13:14:40 -0700

    refactoring some functions, creating readme

---
+++ R/plot_circles.R
@@ -0,0 +1,29 @@
+GeomPlotCircles <- ggplot2::ggproto(
+  "GeomPlotCircles", ggforce::GeomCircle,
+  required_aes = ggforce::GeomCircle$required_aes,
+  default_aes = ggforce::GeomCircle$default_aes,
+  setup_data = function(data, params) {
+    split_circles(data)
+  }
+)
+
+#' FIA subplot circles of arbitrary radius
+#'
+#' This function creates the four FIA subplot circles of a given radius,
+#' and is typically used in the convenience wrappers `geom_macroplot()`,
+#' `geom_midplot()`, and `geom_microplot()`, which set the radius to the
+#' specified size for each subplot type.
+#'
+#' @export
+geom_plot_circles <- function(mapping = NULL, data = NULL,
+  stat = ggforce::StatCircle, position = "identity", n = 360, expand = 0,
+  radius = 0, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
+) {
+  # FIXME some aesthetics should not be possible (e.g., position_jitter does
+  # not work)
+  ggplot2::layer(
+    data = data, mapping = mapping, stat = stat, geom = GeomPlotCircles,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = rlang::list2(n = n, na.rm = na.rm, ...)
+  )
+}
--- R/split.R
+++ R/split.R
@@ -1,5 +1,7 @@
-#' Splits a set of coordinates into their subplot locations
-split_subplots <- function(data) {
+#' Splits circles into the FIA arrangement
+#'
+#' @keywords internal
+split_circles <- function(data) {
   n_elements <- length(unique(data$group))
   n_vertices <- nrow(data) / n_elements
 
@@ -14,6 +16,37 @@ split_subplots <- function(data) {
     dplyr::group_by(.data$group) %>%
     dplyr::slice(rep(seq(dplyr::n()), 4)) %>%
     dplyr::mutate(subgroup = rep(1:4, each = n_vertices)) %>%
+    dplyr::left_join(shift, by = "subgroup") %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      group = paste0(.data$group, ".", .data$subgroup),
+      x = .data$x + .data$diff_x, y = .data$y + .data$diff_y
+    ) %>%
+    dplyr::mutate(group = as.numeric(.data$group)) %>%
+    as.data.frame()
+
+  replicated
+}
+
+#' Splits points into the FIA arrangement
+#'
+#' @keywords internal
+split_points <- function(data) {
+  n_elements <- nrow(data)
+
+  shift <- data.frame(
+    subgroup = c(1, 2, 3, 4),
+    diff_x = c(0, 0, 60 * sqrt(3), -60 * sqrt(3)),
+    diff_y = c(0, 120, -60, -60)
+  )
+
+  # TODO could be simplified into one function, and resolve with split_circles
+  replicated <- data %>%
+    dplyr::mutate(id = 1:nrow(.)) %>%
+    dplyr::rename(x = x0, y = y0) %>%
+    dplyr::group_by(.data$id) %>%
+    dplyr::slice(rep(seq(dplyr::n()), 4)) %>%
+    dplyr::mutate(subgroup = 1:4) %>%
     dplyr::left_join(shift, by = "subgroup") %>%
     dplyr::ungroup() %>%
     dplyr::mutate(
