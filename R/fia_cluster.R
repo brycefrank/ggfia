3a2ec86004e20b93738a617aeb92788ad05887ad
Frank <12001003032399@FEDIDCARD.GOV>
2024-05-02 10:26:45 -0700

    adding subplot type helper fns

--- R/fia_cluster.R
+++ R/fia_cluster.R
@@ -3,9 +3,6 @@ GeomFiaCluster <- ggplot2::ggproto(
   required_aes = ggforce::GeomCircle$required_aes,
   default_aes = ggforce::GeomCircle$default_aes,
   setup_data = function(data, params) {
-    # FIXME this causes issues when sent to GeomShape, becuase the group
-    # assignment has changed, resulting in warnings for first_rows$linewidth
-    # Is there a simpler way to do this further "upstream"? e.g., with a stat?
     split_subplots(data)
   }
 )
@@ -14,7 +11,8 @@ geom_fia_cluster <- function(mapping = NULL, data = NULL,
   stat = ggforce::StatCircle, position = "identity", n = 360, expand = 0,
   radius = 0, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
 ) {
-
+  # FIXME some aesthetics should not be possible (e.g., position_jitter does
+  # not work)
   ggplot2::layer(
     data = data, mapping = mapping, stat = stat, geom = GeomFiaCluster,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
--- R/subplot_types.R
+++ R/subplot_types.R
@@ -1,16 +1,65 @@
-GeomMacroplot <- ggplot2::ggproto(
-  "GeomMacroplot", GeomFiaCluster,
-  required_aes = c("x0", "y0"),
-  default_aes = GeomFiaCluster$default_aes
+StatMacroplot <- ggproto(
+  "StatMacroplot", Stat,
+  compute_panel = function(data, scales, n = 360) {
+    data$r <- 58.9 # TODO incorporate arbitrary units
+    ggforce::StatCircle$compute_panel(data, scales, n = n)
+  },
+  required_aes = c("x0", "y0")
 )
 
+
 geom_macroplot <- function(mapping = NULL, data = NULL,
-  stat = ggforce::StatCircle, position = "identity", n = 360, expand = 0,
+  stat = StatMacroplot, position = "identity", n = 360, expand = 0,
   radius = 0, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
 ) {
 
   ggplot2::layer(
-    data = data, mapping = mapping, stat = stat, geom = GeomMacroplot,
+    data = data, mapping = mapping, stat = stat, geom = GeomFiaCluster,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = rlang::list2(n = n, na.rm = na.rm, ...)
+  )
+}
+
+StatMidplot <- ggproto(
+  "StatMidplot", Stat,
+  compute_panel = function(data, scales, n = 360) {
+    data$r <- 24 # TODO incorporate arbitrary units
+    ggforce::StatCircle$compute_panel(data, scales, n = n)
+  },
+  required_aes = c("x0", "y0")
+)
+
+
+geom_midplot <- function(mapping = NULL, data = NULL,
+  stat = StatMidplot, position = "identity", n = 360, expand = 0,
+  radius = 0, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
+) {
+
+  ggplot2::layer(
+    data = data, mapping = mapping, stat = stat, geom = GeomFiaCluster,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = rlang::list2(n = n, na.rm = na.rm, ...)
+  )
+}
+
+StatMicroplot <- ggproto(
+  "StatMicroplot", Stat,
+  compute_panel = function(data, scales, n = 360) {
+    data$r <- 6.8 # TODO incorporate arbitrary units
+    data$x0 <- data$x0 + 12
+    ggforce::StatCircle$compute_panel(data, scales, n = n)
+  },
+  required_aes = c("x0", "y0")
+)
+
+
+geom_microplot <- function(mapping = NULL, data = NULL,
+  stat = StatMicroplot, position = "identity", n = 360, expand = 0,
+  radius = 0, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
+) {
+
+  ggplot2::layer(
+    data = data, mapping = mapping, stat = stat, geom = GeomFiaCluster,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = rlang::list2(n = n, na.rm = na.rm, ...)
   )
